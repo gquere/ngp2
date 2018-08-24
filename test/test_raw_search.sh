ba12cd4b66ac1890d20ba7fa8a80237b3efff5c7
Guillaume Quéré <none>
2018-08-24 10:33:43 +0200

    [add] Test for raw search

---
+++ test/test_raw_search.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NGP=../ngp_perf
+PATTERN="int"
+RESOURCE=./resources/file_with_ignored_extension.txt
+EXPECT="Found 1 files, 2 lines"
+
+result=$($NGP -r $PATTERN $RESOURCE)
+
+if [ "$result" != "$EXPECT" ]
+then
+    echo "$0 failed"
+    echo "Expected: '$EXPECT'"
+    echo "Got: '$result'"
+    exit -1
+fi
