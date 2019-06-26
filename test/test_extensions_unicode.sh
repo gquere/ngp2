9dd1f2dceb2c998dbbcc71bf28386666a9101353
Guillaume Quéré <none>
2019-06-26 08:19:50 +0200

    [add] Bunch of extension tests

---
+++ test/test_extensions_unicode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NGP=../ngp_perf
+PATTERN="int"
+RESOURCE=./resources/extensions/
+
+EXPECT="Found 1 files, 1 lines"
+result=$($NGP $PATTERN $RESOURCE -o '.été')
+
+if [ "$result" != "$EXPECT" ]
+then
+    echo "$0 failed"
+    echo "Expected: '$EXPECT'"
+    echo "Got: '$result'"
+    exit -1
+fi
+
+echo "$0 OK"
